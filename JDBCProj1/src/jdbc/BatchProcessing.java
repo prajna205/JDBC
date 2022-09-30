df6269bc2f78d469bb96e84286e9a1e10539e601
Prajna <sahooprajna73@gmail.com>
2022-09-30 18:28:27 +0530

    transction management

--- JDBCProj1/src/jdbc/BatchProcessing.java
+++ JDBCProj1/src/jdbc/BatchProcessing.java
@@ -1,9 +1,36 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class BatchProcessing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try(Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","1998");
+				Statement st = con.createStatement();
+				) {
+			if (st != null) {
+				st.addBatch("INSERT INTO STUDENT VALUES (25, 'RAHUL', 'BLR', 54.6)");
+				st.addBatch("UPDATE STUDENT SET AVG = AVG + 10 WHERE SADDR = 'sbp' ");
+				st.addBatch("DELETE FROM STUDENT WHERE SNO> 10");
+				int[] result = st.executeBatch();	
+				//process the result
+				int total =0;
+				for (int i = 0; i < result.length; i++) 
+					total = total + result[i];					
+				
+				System.out.println("Total Number of records changed : "+ total);
+			}//if
+		}//try
+		catch (SQLException se) {
+			se.printStackTrace();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
